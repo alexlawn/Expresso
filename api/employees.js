47f9d89d90f2abed8a3696a0dd623a62d6b37fbe
Alex <alexlawn333@gmail.com>
2021-07-28 17:40:55 +0100

    GET for /api/employees

--- api/employees.js
+++ api/employees.js
@@ -4,7 +4,20 @@ const employeesRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+employeesRouter.get('/', (req, res, next) => {
+    db.all(`SELECT * FROM Employee WHERE Employee.is_current_employee = 1`,
+    (err, employees) => {
+        if(err) {
+            next(err);
+        } else {
+            res.status(200).json({employees: employees});
+        }
+    });
+});
 
+employeesRouter.post('/', (req, res, next) => {
+    //
+})
 
 
 module.exports = employeesRouter;
