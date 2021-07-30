e87c0631165cc6a84f1dad8aa6e1f69af086a5ee
Alex <alexlawn333@gmail.com>
2021-07-30 15:29:52 +0100

    creating GET /api/employees/:employeeId/timesheets

--- api/timesheets.js
+++ api/timesheets.js
@@ -1,8 +1,26 @@
 const express = require('express');
-const timesheetsRouter = express.Router();
+const timesheetsRouter = express.Router({mergeParams: true});
 
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+
+
+timesheetsRouter.get('/', (req, res, next) => {
+    db.all(`SELECT * FROM Timesheet WHERE Timesheet.employee_id = $employeeId`,
+    {
+        $employeeId: req.params.employeeId
+    },
+    (err, timesheets) => {
+        if(err) {
+            next(err);
+        } else {
+            res.status(200).json({timesheets: timesheets});
+        }
+    });
+});
+
+
 
 
 
