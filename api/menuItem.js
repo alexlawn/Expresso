9a2624f119dac4e953f3decd13de957193421ab5
Alex <alexlawn333@gmail.com>
2021-08-02 17:03:55 +0100

    creating GET /api/menus/:menuId/menu-items

--- api/menuItem.js
+++ api/menuItem.js
@@ -4,3 +4,32 @@ const menuItemRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+menuItemRouter.param('menuId', (req, res, next, menuId) => {
+    db.get(`SELECT * FROM MenuItem WHERE MenuItem.id = $menuId`,
+    {
+        $menuId: menuId
+    },
+    (err, menuItem) => {
+        if(err) {
+            next(err);
+        } else if (menuItem) {
+            next();
+        } else {
+            res.sendStatus(404);
+        }
+    });
+});
+
+menuItemRouter.get('/', (req, res, next) => {
+    db.all(`SELECT * FROM MenuItem WHERE MenuItem.menu_id = $menuId`,
+    {
+        $menuId: req.params.menuId
+    },
+    (err, menuItem) => {
+        if(err) {
+            next(err);
+        } else {
+            res.status(200).json({menuItem: menuItem});
+        }
+    });
+});
