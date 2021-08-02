cf344f3dd75d395ed67af6945e94afd5d1bcc901
Alex <alexlawn333@gmail.com>
2021-08-02 13:46:58 +0100

    creating PUT /api/menus/:menuId

--- api/menus.js
+++ api/menus.js
@@ -4,6 +4,22 @@ const menusRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+menusRouter.param('menuId', (req, res, next, menuId) => {
+    db.get('SELECT * FROM Menu WHERE Menu.id = $menuId', 
+    {
+        $menuId: menuId
+    }, (err, menu) => {
+        if(err){
+            next(err);
+        } else if(menu) {
+            req.menu = menu;
+            next();
+        } else {
+            res.sendStatus(404);
+        }
+    });
+});
+
 menusRouter.get('/', (req, res, next) => {
     db.all(`SELECT * FROM Menu`,
     (err, menus) => {
@@ -13,6 +29,10 @@ menusRouter.get('/', (req, res, next) => {
             res.status(200).json({menus: menus});
         }
     });
+});
+
+menusRouter.get('/:menuId', (req, res, next) => {
+    res.status(200).json({menu: req.menu});
 });
 
 menusRouter.post('/', (req, res, next) => {
@@ -36,6 +56,29 @@ menusRouter.post('/', (req, res, next) => {
         });
     }
 });
+
+menusRouter.put('/:menuId', (req, res, next) => {
+    const title = req.body.title;
+    if(!title) {
+        return res.sendStatus(400);
+    } else {
+        db.run(`UPDATE Menu SET title = $title`,
+        {
+            $title: title
+        },
+        (err) => {
+            if(err) {
+                next(err);
+            } else {
+                db.get(`SELECT * FROM Menu WHERE Menu.id = ${req.params.menuId}`,
+                (err, updatedMenu) => {
+                    res.status(200).json({menu: updatedMenu});
+                });
+            }
+        });
+    }
+});
+
 
 
 module.exports = menusRouter;
